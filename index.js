49bd03bd7cb917e406787885a93ec1cc4a1ff2d8
RichardEsquivel <rlesquivel@gmail.com>
2019-10-09 14:20:19 -0500

    .

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //creating router
 const express = require('express')
-
+const vehsRouter = require('./cars/car-router')
 //create server
 const server = express();
 
 //create middleware express.json to parse json body
 server.use(express.json());
+
+server.use('/vehs', vehsRouter);
 
 //declare value of port
 const port = 4000;
